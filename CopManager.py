384b21efd5c421826f73a5c280d79a128e18e75a
Leo927 <asd347925290@gmail.com>
2020-10-19 18:36:17 -0600

    changed module names to lower case

--- CopManager.py
+++ CopManager.py
@@ -9,9 +9,10 @@ Created on Sun Sep 13 17:53:45 2020
 import networkx as nx
 import matplotlib.pyplot as plt
 import copy
-import TreeDrawer
-import TreeBuilder
+import treedrawer
+import treebuilder
 import constant
+import random
 
 def reverseList(tree,root):
     reversedList = []
@@ -50,14 +51,14 @@ def isKBranching(tree, node, k):
         
     
 def joinByU(rooted_tree):
-    return TreeBuilder.rooted_tree(nx.join([(rooted_tree.tree, rooted_tree.root), (rooted_tree.tree, rooted_tree.root)]),0)
+    return treebuilder.rooted_tree(nx.join([(rooted_tree.tree, rooted_tree.root), (rooted_tree.tree, rooted_tree.root)]),0)
     
     
 def subForest(tree, node, distance:int):
     distance += 1
     digraph = nx.bfs_tree(tree,node)
     children = nx.descendants_at_distance(digraph, node, distance)
-    return [TreeBuilder.rooted_tree(nx.bfs_tree(digraph,child),child) for child in children]
+    return [treebuilder.rooted_tree(nx.bfs_tree(digraph,child),child) for child in children]
     
 
 #The minimum cop number to cover a tree
@@ -72,6 +73,33 @@ def c1Star(forest):
         result = max(result, c1(tree))
     return result
 
+
+    
+def findConnectedNodes(rooted_tree, node, data=False):
+    return dict( nx.bfs_successors(rooted_tree.tree,node,1))[node]  
+
+def getWeight(rooted_tree, node):
+    return rooted_tree.tree.nodes(True)[node]['weight']
+
+def findLongestEqualWeightPath(rooted_tree, parent, node):
+    connectedNodes = findConnectedNodes(rooted_tree, node)
+    
+    longestPath = []    
+    for cNode in connectedNodes:
+        if cNode == parent:
+            continue
+        if getWeight(rooted_tree, cNode) == getWeight(rooted_tree, node):
+            path = findLongestEqualWeightPath(rooted_tree, node, cNode)
+            if len(path)>len(longestPath):
+                longestPath = path
+    longestPath.append(node)
+    return longestPath
+    #find all nodes that are connected to node
+    
+    #foreach nodes that has the same weight
+        #call this function with these nodes and store the return value
+    #return the longest list among the returned list and empty list
+    pass
 
 
     
